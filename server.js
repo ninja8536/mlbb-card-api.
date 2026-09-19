24e1cf726b080791021e57a22c0766bc4c90d1a8
Mommy minsi <61105990+ninja8536@users.noreply.github.com>
2026-09-19 06:25:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,23 +17,23 @@ app.post('/image', async (req, res) => {
         });
 
         const page = await browser.newPage();
-        await page.setViewport({ width: 500, height: 400 });
+        await page.setViewport({ width: 1200, height: 1200, deviceScaleFactor: 2 });
         await page.setContent(html);
         const element = await page.$('.mlbb-card');
         const imageBuffer = await element.screenshot({ type: 'png' });
         await browser.close();
 
         const form = new FormData();
-// Send the buffer to Discord and name it as a .png file
-form.append('file', imageBuffer, { filename: 'mlbbcard.png' });
+        // Send the buffer to Discord and name it as a .png file
+        form.append('file', imageBuffer, { filename: 'mlbbcard.png' });
 
-// Send the image to the Webhook channel
-const discordRes = await axios.post("https://discord.com/api/webhooks/1550654446822887555/MDXDHa2_fEzkXN9SGA5iffb8Xt30D0Q_6iRvpA5y2BJBM_TK4xv2r4YlwmqNbx4i7aps?wait=true", form, {
-    headers: form.getHeaders()
-});
+        // Send the image to the Webhook channel
+        const discordRes = await axios.post("https://discord.com/api/webhooks/1550654446822887555/MDXDHa2_fEzkXN9SGA5iffb8Xt30D0Q_6iRvpA5y2BJBM_TK4xv2r4YlwmqNbx4i7aps?wait=true", form, {
+            headers: form.getHeaders()
+        });
 
-// Grab the native Discord CDN link and send it back to BotGhost
-res.json({ url: discordRes.data.attachments[0].url });
+        // Grab the native Discord CDN link and send it back to BotGhost
+        res.json({ url: discordRes.data.attachments[0].url });
 
     } catch (error) {
         console.error(error);
